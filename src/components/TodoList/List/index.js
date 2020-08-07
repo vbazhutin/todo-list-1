81bcc79d6cb7fb00b5b4f93ede762a10ce988aff
Manav Misra <manavm@visionwebsoft.com>
2020-08-07 15:10:45 -0500

    :construction: Add line-through CSS if task is completed
    :sparkles: Toggle completed and incomplete
    
    Removing 'const' and using 'target.parentEementID' directly.

--- src/components/TodoList/List/index.js
+++ src/components/TodoList/List/index.js
@@ -1,11 +1,13 @@
 import PropTypes from "prop-types"
 import React from "react"
 
+import "./List.css"
+
 export const List = ({ todos, handler }) => {
   return (
     <ol>
-      {todos.map(({ id, text }) => (
-        <li key={id} data-id={id}>
+      {todos.map(({ id, completed, text }) => (
+        <li key={id} data-id={id} className={completed ? "completed" : null}>
           {text} <input type="checkbox" onClick={handler} />
         </li>
       ))}
