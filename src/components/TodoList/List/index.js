afc795176676ae06bc212efc5e489fd7d3c84da7
Manav Misra <manavm@visionwebsoft.com>
2020-08-26 15:46:31 -0500

    :lipstick: Put trash can on same line with proper spacing

--- src/components/TodoList/List/index.js
+++ src/components/TodoList/List/index.js
@@ -10,9 +10,12 @@ export const List = ({ todos, checkboxHandler, trashHandler }) => {
   const todosCount = todos.length
   const completedCount = todos.filter(({ completed }) => completed).length
 
-  let className = "has-background-dark has-text-centered has-text-weight-bold my-4 py-2"
+  let className =
+    "has-background-dark has-text-centered has-text-weight-bold my-4 py-2"
   className +=
-    completedCount / todosCount < 0.5 ? " has-text-warning" : " has-text-success"
+    completedCount / todosCount < 0.5
+      ? " has-text-warning"
+      : " has-text-success"
 
   return (
     <Fragment>
@@ -26,12 +29,14 @@ export const List = ({ todos, checkboxHandler, trashHandler }) => {
 
       <ul>
         {todos.map(({ id, completed, text }) => (
-          <li key={id} data-id={id} className={completed ? "columns completed" : "columns"}>
-            <div className="column is-one-fifth">{text} <input type="checkbox" onClick={checkboxHandler} /></div>
-
-            <div className="column">
-              {/* TODO Put trashcan on same line */}
-              <FontAwesomeIcon icon={faTrash} className="has-text-danger" onClick={trashHandler}/>
+          <li key={id} data-id={id} className={completed ? "completed" : null}>
+            <div>
+              {text} <input type="checkbox" onClick={checkboxHandler} />
+              <FontAwesomeIcon
+                icon={faTrash}
+                className="has-text-danger ml-2"
+                onClick={trashHandler}
+              />
             </div>
           </li>
         ))}
