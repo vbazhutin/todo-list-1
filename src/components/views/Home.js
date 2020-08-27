cab9e959499b92ac38ef01f4c0c8ec314dbb386b
Manav Misra <manavm@visionwebsoft.com>
2020-08-26 22:40:32 -0500

    :speech_balloon: Add icon links in footer

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,5 +1,16 @@
 import React from "react"
 
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons"
+
+library.add(fab, faGithub, faLinkedin, faTwitter)
+
 export const Home = () => (
   <>
     <div className="hero is-fullheight has-text-centered">
@@ -30,5 +41,41 @@ export const Home = () => (
       <div className="divider">Let&apos;s Connect!</div>
     </div>
 
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <strong>Todo List by&nbsp;</strong>
+          <a href="https://codefinity.tech" target="_blank" rel="noreferrer">
+            CodeFinity
+          </a>
+        </p>
+        <div className="level">
+          <a
+            href="https://github.com/manavm1990"
+            target="_blank"
+            rel="noreferrer"
+            className="level-item mx-2"
+          >
+            <FontAwesomeIcon icon={["fab", "github"]} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/manavm1990/"
+            target="_blank"
+            rel="noreferrer"
+            className="level-item mx-2"
+          >
+            <FontAwesomeIcon icon={["fab", "linkedin"]} />
+          </a>
+          <a
+            className="level-item mx-2"
+            href="https://twitter.com/GoCodeFinity"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={["fab", "twitter"]} />
+          </a>
+        </div>
+      </div>
+    </footer>
   </>
 )
