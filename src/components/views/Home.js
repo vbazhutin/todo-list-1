4d834112952b4bb71ec4529e2b6ad0dbc4f6b800
Manav Misra <manavm@visionwebsoft.com>
2020-08-26 20:28:26 -0500

    :lipstick: Add and style hero with title and bg image
    Removing :fire: unused 'wrapper' class

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 export const Home = () => (
-  <section className="hero is-success is-fullheight">
+  <div className="hero is-fullheight has-text-centered">
     <div className="hero-body">
       <div className="container">
-        <h1 className="title">Fullheight title</h1>
-        <h2 className="subtitle">Fullheight subtitle</h2>
+        <h1 className="title">Todo List!</h1>
+        <div className="flex flex--align-center flex--column">
+          <button className="button is-primary my-2">Get Started</button>
+          <button className="button is-small my-2">Login</button>
+        </div>
       </div>
     </div>
-  </section>
+  </div>
 )
