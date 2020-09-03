92e28fec495bdd837e3456bf372ac0ac161ba67c
Vlad <vladimir.bazhutin@gmail.com>
2020-09-03 11:28:34 -0500

    Add logic to login, register or reset password for acc using firebash/auth

--- client/src/components/views/Login/index.js
+++ client/src/components/views/Login/index.js
@@ -1,33 +1,43 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
-import { Formik, Form, Field, ErrorMessage } from "formik"
-import { useLocation } from 'react-router-dom'
-import * as Yup from 'yup'
+import { Formik, Field, Form, ErrorMessage } from "formik"
 
-import auth from 'auth'
+import { useHistory, useLocation } from "react-router-dom"
 
-import { Options } from './Options'
+import * as Yup from "yup"
+
+import auth from "auth"
+
+import { Options } from "./Options"
 
 export const Login = () => {
+  const history = useHistory()
   const location = useLocation()
+
   const [forgotMode, setForgotMode] = useState(false)
   const [loginMode, setLoginMode] = useState(location.search.includes("login"))
 
-  const AccHandler = (event) => {
-    const text = event.target.innerText
-    if (text.includes('Forgot')) {
-      setForgotMode(prev => !prev)
-
+  const handleToggle = (event) => {
+    if (event.target.textContent.includes("Forgot")) {
+      setForgotMode(true)
     } else {
       setForgotMode(false)
-      setLoginMode(prevLogin => !prevLogin)
+      setLoginMode((prev) => !prev)
     }
   }
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        history.push("/todos", { currentUser: user.email })
+      }
+    })
+  }, [history])
 
   return (
     <section className="box center mt-4 section">
       <h2 className="has-text-centered title">
-        {loginMode ? "Login" : "Create account"}
+        {loginMode ? "Login" : "Create Account"}
       </h2>
       <Formik
         initialValues={{
@@ -36,21 +46,56 @@ export const Login = () => {
           pass: "",
         }}
         validationSchema={Yup.object({
-          name: !loginMode && Yup.string().required("Name is required"),
-          email: Yup.string().email("Valid email required").required("Email is required"),
-          pass: !forgotMode && Yup.string().min(6).required("Password is required")
+          name: !loginMode && Yup.string().required("Name is required!"),
+          email: Yup.string()
+            .email("Invalid email address!")
+            .required("Email is required!"),
+          pass:
+            !forgotMode && Yup.string().min(6).required("Pass is required!"),
         })}
-        onSubmit={(values, { setSubmitting }) => {
-          // This is like utility fxn. that gathers all values
-          console.log("submission", values)
-          setSubmitting(false)
+        onSubmit={({ name, email, pass }, { setSubmitting }) => {
+          if (forgotMode) {
+            auth
+              .sendPasswordResetEmail(email)
+              .then((res) => {
+                // TODO: Create a notification to tell them to check their ✉️
+              })
+              .catch((err) => {
+                console.error(err)
+              })
+          } else if (loginMode) {
+            auth
+              .signInWithEmailAndPassword(email, pass)
+              .then(() => {
+                setSubmitting(false)
+                history.push("/todos", { currentUser: email })
+              })
+              .catch((err) => {
+                console.error(err)
+              })
+          } else {
+            auth
+              .createUserWithEmailAndPassword(email, pass)
+              .then(() => {
+                setSubmitting(false)
+                // TODO: Use our api to send 'name' and any other deets over to Mongo
+                // After this is sent to our Mongo...
+                // try-catch
+                // history.push("/todos", { currentUser: email })
+              })
+              .catch((err) => {
+                console.log(err)
+              })
+          }
         }}
       >
         {({ isSubmitting }) => (
           <Form>
             {!loginMode ? (
               <div className="field">
-                <label htmlFor="name" className="ml-2">Name</label>
+                <label htmlFor="name" className="ml-2">
+                  Name
+                </label>
                 <div className="control mx-2 my-1">
                   <Field name="name" type="text" />
                   <p className="help is-danger">
@@ -61,33 +106,42 @@ export const Login = () => {
             ) : null}
 
             <div className="field">
-              <label htmlFor="email" className="ml-2">Email</label>
+              <label htmlFor="email" className="ml-2">
+                Email
+              </label>
               <div className="control mx-2 my-1">
                 <Field name="email" type="email" />
-                <p className="help is-danger">
-                  <ErrorMessage name="email" />
-                </p>
+                <ErrorMessage name="email" />
               </div>
             </div>
 
             {!forgotMode ? (
               <div className="field">
-                <label htmlFor="pass" className="ml-2">Password</label>
+                <label htmlFor="pass" className="ml-2">
+                  Password
+                </label>
                 <div className="control mx-2 my-1">
                   <Field name="pass" type="password" />
-                  <p className="help is-danger">
-                    <ErrorMessage name="pass" />
-                  </p>
+                  <ErrorMessage name="pass" />
                 </div>
               </div>
             ) : null}
 
-
-            <button type="submit" className="button is-success ml-2 mt-2" disabled={isSubmitting}>Submit</button>
+            <button
+              type="submit"
+              className="button is-success ml-2 mt-2"
+              disabled={isSubmitting}
+            >
+              Submit
+            </button>
           </Form>
-          )}
+        )}
       </Formik>
-      <Options loginMode={loginMode} forgotMode={forgotMode} AccHandler={AccHandler} />
+      <Options
+        forgotMode={forgotMode}
+        loginMode={loginMode}
+        handler={handleToggle}
+      />
     </section>
   )
 }
